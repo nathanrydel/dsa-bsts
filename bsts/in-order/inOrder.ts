cc65ce5c16f762647d05124c835634921949fba4
Nathan Rydel <nathanrydel@gmail.com>
2024-02-29 03:21:30 -0700

    inOrder (but recursive) implementation
    - tests passing

--- bsts/in-order/inOrder.ts
+++ bsts/in-order/inOrder.ts
@@ -4,10 +4,41 @@ import { BNodeNum } from "../common/bst";
  * Returns an array of visited nodes. */
 
 function inOrder(node: BNodeNum | null): number[] {
-  return [42];
+
+  // ITERATIVE APPROACH
+  // if (node === null) return [];
+
+  // const dfsStack = new Stack<BNodeNum>([node]);
+  // const toVisit: number[] = [];
+
+  // while (!dfsStack.isEmpty()) {
+  //   let current = dfsStack.pop();
+
+  //   if (current.left) {
+  //     dfsStack.push(current.left);
+  //     toVisit.push(current.left.val);
+  //     if (current) {
+  //       dfsStack.push(current);
+  //       toVisit.push(current.val);
+  //     }
+  //     if (current.right) {
+  //       dfsStack.push(current.right);
+  //       toVisit.push(current.right.val);
+  //     }
+  //   }
+  // }
+
+  // return toVisit;
+
+  // RECURSIVE APPROACH
+  if (node === null) return [];
+
+  return [
+    ...inOrder(node.left),
+    node.val,
+    ...inOrder(node.right)
+  ];
 }
-
-
 
 /** inOrderAccum(): Traverse the BST using in-order DFS.
  * Returns an array of visited nodes.
