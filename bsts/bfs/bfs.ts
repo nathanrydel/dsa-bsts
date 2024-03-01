2ab6178d271f277b9acf9b17842b8ebf32f3c163
Eric Huie <erichuie97@gmail.com>
2024-03-01 11:57:35 -0800

    added bfs search for BSTs

--- bsts/bfs/bfs.ts
+++ bsts/bfs/bfs.ts
@@ -10,6 +10,20 @@ function bfs(node: BNodeNum | null): number[] {
   const queue = new Queue<BNodeNum>([node]);
   const valsReturn: number[] = [];
 
+  // let currNode = queue.dequeue();
+  // valsReturn.push(queue.dequeue().val);
+
+  while(!queue.isEmpty()){
+    let currNode = queue.dequeue();
+    valsReturn.push(currNode.val);
+    if(currNode.left !== null){
+      queue.enqueue(currNode.left);
+    }
+    if(currNode.right !== null){
+      queue.enqueue(currNode.right);
+    }
+  }
+
   return valsReturn;
 
 }
