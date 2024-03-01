423bc24a71b4af4f1aa4e70cc5e621e4a307a325
Eric Huie <erichuie97@gmail.com>
2024-02-29 17:54:18 -0800

    working on breadth first search of bst

--- bsts/bfs/bfs.ts
+++ bsts/bfs/bfs.ts
@@ -1,11 +1,17 @@
 import { BNodeNum } from "../common/bst";
+import { Queue } from "../common/queue";
 
 
 /** bfs(): Traverse the BST using BFS.
  * Returns an array of visited nodes. */
 
 function bfs(node: BNodeNum | null): number[] {
-  return [42];
+  if(node === null) return [];
+  const queue = new Queue<BNodeNum>([node]);
+  const valsReturn: number[] = [];
+
+  return valsReturn;
+
 }
 
 export { bfs };
